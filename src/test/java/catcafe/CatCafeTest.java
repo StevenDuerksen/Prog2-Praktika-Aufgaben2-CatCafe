41c61105a9f7c911a76400ad48bde93a6aba462a
StevenDuerksen <stevenduerksen@gmail.com>
2026-05-09 20:15:50 +0200

    configure gradle, add JUnit tests

---
+++ src/test/java/catcafe/CatCafeTest.java
@@ -0,0 +1,96 @@
+package catcafe;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class CatCafeTest {
+
+    @Test
+    void shouldHaveZeroCatsWhenCreated(){
+        //given
+        CatCafe cafe = new CatCafe();
+
+        //when
+        long result = cafe.getCatCount();
+
+        //then
+        assertEquals(0, result);
+    }
+
+    @Test
+    void shouldAddOneCat(){
+        //given
+        CatCafe cafe = new CatCafe();
+        FelineOverLord cat = new FelineOverLord("Giga Cat", 5);
+
+        //when
+        cafe.addCat(cat);
+
+        //then
+        assertEquals(1, cafe.getCatCount());
+    }
+
+    @Test
+    void shouldFindCatCount(){
+        //given
+        CatCafe cafe = new CatCafe();
+        FelineOverLord cat1 = new FelineOverLord("Omega Cat", 7);
+        FelineOverLord cat2 = new FelineOverLord("Sigma Cat", 4);
+        FelineOverLord cat3 = new FelineOverLord("Phi Cat", 100);
+        cafe.addCat(cat1);
+        cafe.addCat(cat2);
+        cafe.addCat(cat3);
+
+        //when
+        long result = cafe.getCatCount();
+
+        //then
+        assertEquals(3, result);
+    }
+
+    @Test
+    void shouldFindCatByName(){
+        //given
+        CatCafe cafe = new CatCafe();
+        FelineOverLord cat = new FelineOverLord("Omega Cat", 7);
+        cafe.addCat(cat);
+
+        //when
+        FelineOverLord result = cafe.getCatByName("Omega Cat");
+
+        //then
+        assertEquals(cat, result);
+    }
+
+    @Test
+    void shouldReturnCatWithinWeightRange(){
+        //given
+        CatCafe cafe = new CatCafe();
+        FelineOverLord cat1 = new FelineOverLord("Omega Cat", 7);
+        FelineOverLord cat2 = new FelineOverLord("Sigma Cat", 4);
+        FelineOverLord cat3 = new FelineOverLord("Phi Cat", 100);
+        cafe.addCat(cat1);
+        cafe.addCat(cat2);
+        cafe.addCat(cat3);
+
+        //when
+        FelineOverLord result = cafe.getCatByWeight(3,9);
+
+        //then
+        assertEquals(cat2, result);
+    }
+
+    @Test
+    void shouldReturnNullWhenNameDoesNotExist(){
+        //given
+        CatCafe cafe = new CatCafe();
+        FelineOverLord cat = new FelineOverLord("Omega Cat", 7);
+        cafe.addCat(cat);
+
+        //when
+        FelineOverLord result = cafe.getCatByName("Hund");
+
+        //then
+        assertNull(result);
+    }
+}
---
+++ src/test/java/catcafe/FelineOverLordTest.java
@@ -0,0 +1,89 @@
+package catcafe;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FelineOverLordTest {
+
+    /**
+     * Warum sollte ich die return values von FelineOverLord testen wenn ich diese
+     * quasi in CatCafeTest auch überprüfe? Erschien mir am Anfang etwas redundant
+     * aber in CatCafeTest überprüfe ich nur ob die Verwaltung von CatCafe funktioniert
+     * und nicht ob die einzelnen Objekte von FelineOverLord funktionieren.
+     */
+
+    @Test
+    void shouldReturnName(){
+        //given
+        FelineOverLord cat = new FelineOverLord("Alpha Cat", 5);
+
+        //when
+        String result = cat.name();
+
+        //then
+        assertEquals("Alpha Cat", result);
+    }
+
+    @Test
+    void shouldReturnWeight(){
+        //given
+        FelineOverLord cat = new FelineOverLord("Alpha Cat", 5);
+
+        //when
+        int result = cat.weight();
+
+        //then
+        assertEquals(5, result);
+    }
+
+    @Test
+    void shouldReturnNegativeValueWhenCatIsLighter(){
+        //given
+        FelineOverLord cat1 = new FelineOverLord("Omega Cat", 7);
+        FelineOverLord cat2 = new FelineOverLord("Feuerzeug", 4);
+
+        //when
+        int result = cat2.compareTo(cat1);
+
+        //then
+        assertTrue(result < 0, "Cat should be lighter");
+    }
+
+    @Test
+    void shouldReturnPositiveValueWhenCatIsHeavier(){
+        //given
+        FelineOverLord cat1 = new FelineOverLord("Omega Cat", 7);
+        FelineOverLord cat2 = new FelineOverLord("Alpha Cat", 4);
+
+        //when
+        int result = cat1.compareTo(cat2);
+
+        //then
+        assertTrue(result > 0, "Cat should be hefty");
+    }
+
+    @Test
+    void shouldReturnCorrectToString(){
+        //given
+        FelineOverLord cat = new FelineOverLord("Alpha Cat", 5);
+
+        //when
+        String result = cat.toString();
+
+        //then
+        assertEquals("FelineOverLord[name=Alpha Cat, weight=5]", result);
+    }
+
+    @Test
+    void shouldStoreLargeWeightCorrectly(){
+        //given
+        FelineOverLord cat = new FelineOverLord("Massive Cat", 999999999);
+
+        //when
+        int result = cat.weight();
+
+        //then
+        assertEquals(999999999, result);
+    }
+}
